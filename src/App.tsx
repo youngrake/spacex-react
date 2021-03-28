84a58c8d26ae191d097a8ba9edf280830f85c547
youngrake <youngrake@protonmail.com>
2021-03-28 21:13:01 +0300

    configuration (eslint, prettier, husky)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-	return <div className='App'></div>;
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
